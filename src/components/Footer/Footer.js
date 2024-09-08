fd5a95899daee300ab6ed51a5979499cf7e84427
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-09-08 15:20:03 +0200

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Created with Love by Paulin
+    </a>
+  </footer>
+)
+
+export default Footer
